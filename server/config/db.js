29a249b989b731dfae4854c47a6f6a9df85c0efa
itzadamx <fcbpro1337@gmail.com>
2021-11-06 15:40:15 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+ "URI": "mongodb+srv://adam:adaM1337@midterm.4wlfj.mongodb.net/Book?retryWrites=true&w=majority"
+};
